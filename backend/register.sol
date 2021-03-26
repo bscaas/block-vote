2f8e7faa3c417f4efc12f570d9feb9dd6a91bac6
Emmanuel947 <53540619+Emmanuel947@users.noreply.github.com>
2021-03-26 10:33:40 +0100

    Registration

---
+++ backend/register.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.16;
+
+contract registerAccount {
+    
+    uint public numAccount;
+    
+    constructor() public {
+        numAccount = 0;
+    }
+    
+    struct account {
+        address addr; 
+        string name;
+        string email;
+    }
+    
+    mapping(uint => account) public accounts;
+    
+    
+    
+    function register (string memory _name, string memory _email) public {
+        accounts[numAccount].addr = msg.sender;
+        accounts[numAccount].name = _name;
+        accounts[numAccount].email = _email;
+        numAccount++;
+    }
+    
+    
+    function getInfo(uint numAccount) public view returns(string memory) {
+        return accounts[numAccount].name;
+    }
+}
