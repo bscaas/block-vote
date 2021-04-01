19466e0d897768b382391ece5744510cdb21864a
Tjad Clark <tjataz@gmail.com>
2021-04-01 23:43:59 +0200

    frontend: Encryption and Decryption

--- frontend/src/util/wallet-util.js
+++ frontend/src/util/wallet-util.js
@@ -1,23 +1,47 @@
+const sigUtil = require('eth-sig-util')
+const ethUtil = require('ethereumjs-util')
+
+
 export default class WalletUtil{
     constructor(){
 
     }
 
     getPublicKey(){
-      return new Promise((r)=>{window.web3.eth.getAccounts().then((accounts)=>{
+      return new Promise((r)=>{
         window.ethereum.request({
           method: 'eth_getEncryptionPublicKey',
-          params: [accounts[0]], // you must have access to the specified account
+          params: [window.ethereum.selectedAddress], // you must have access to the specified account
         }).then((x)=>r(x))
-      })})
+      })
         
     }
 
-    encrypt(){
+    encrypt(msg, encryptionPublicKey){
+      const encryptedMessage = ethUtil.bufferToHex(
+        Buffer.from(
+          JSON.stringify(
+            sigUtil.encrypt(
+              encryptionPublicKey,
+              { data: msg },
+              'x25519-xsalsa20-poly1305'
+            )
+          ),
+          'utf8'
+        )
+      );
 
+      return encryptedMessage;
     }
 
-    decrypt(){
-
+    /*
+      TODO: Decrypt multiple messages simultaneously
+    */
+    decrypt(encrypted_msg){
+      return window.ethereum
+      .request({
+        method: 'eth_decrypt',
+        params: [encrypted_msg, window.ethereum.selectedAddress],
+      })
     }
 }
