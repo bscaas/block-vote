21cf62bf7be6312f103321d02e471ca5985b99cc
Donkross360 <mart2great@yahoo.com>
2021-04-04 20:25:22 +0100

    Breaking the Fragments into candidates & votes

--- backend/ElectionTally.sol
+++ backend/ElectionTally.sol
@@ -6,24 +6,50 @@ contract ElectionTally {
     uint public totalCandidateVotes;
     mapping (bytes32 => uint256) private votesReceived;
 
-    constructor(bytes32[] memory _votesRecieved) public {
-        votesReceived = _votesRecieved;
+    constructor(bytes32[] memory _vote) public {
+        votesReceived = _vote;
     }
 
-    function submitVoteFragments(String election_id, VoteFragment[] fragments) private view returns (uint256) {
+    function submitVoteFragments(string election_id, VoteFragment[] fragments) private view returns (uint256) {
+        
+        for (uint i = 0; i < fragments.length; i++) {
+            VoteFragment fragment = fragments[i];
+            Vote vote = votes[fragment.vote_id];
+            if (!vote) {
+                vote = new Vote();
+            }
+
+            vote.candidate_key [fragment.index] = fragment.candidate_key_fragment;
+            votes[frgment.vote_id] = vote;
+        
+        }
+
         require(!votingEnded);
         require(validCandidate());
-        require(votesReceived[] < ~uint256(0));
+        require(votesReceived[vote] < ~uint256(0));
         require(totalCandidateVotes < ~uint256(0));
 
-        votesReceived[] += 1;
-        totalCandidateVotes[] += 1;
+        votesReceived[vote] += 1;
+        totalCandidateVotes[vote.candidate_key] += 1;
+        return votesReceived;
+
+    }
+
+    function validCandidate (voteFragment[] fragments) private view returns (bool) {
+        vote.candidate_key[fragment.index] == fragment.candidate_key_fragment;
     }
 
 
-    function getTotalCandidateVotes(bytes32 candidate) view public returns (uint256) {
-        require(votingEnded); 
-        return votesReceived[candidate];
-    }  
+    function getTotalCandidateVotes(voteFragment[] fragments) view public returns (uint256) {
+        require(votingEnded);
+
+        return totalCandidateVotes;
+    }
+
+    
+    function clearVotesFragments(string memory vote) public{
+        delete messages[vote_id][msg.sender];
+    }
+  
     
 }
