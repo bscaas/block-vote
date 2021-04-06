3eb76f42798be10ce96fb2e7e100efd7961d943d
Tjad Clark <tjataz@gmail.com>
2021-04-06 21:53:07 +0200

    #9 backend: Initial Election contract

---
+++ backend/ElectionContrat.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract ElectionContract{
+
+    mapping(string=>Election) elections;
+
+    struct Election{
+        string id;
+        string name;
+        string phase;
+        string elected_candidate_id;
+    }
+    
+    modifier electionExists(string memory id){
+        require(bytes(elections[id].id).length > 0 , "Election does not exist.");
+        _;
+    }
+    
+    modifier electionNotExists(string memory id){
+        require(bytes(elections[id].id).length == 0 , "Election already exists.");
+        _;
+    }
+    
+    function createElection(Election memory election) public electionNotExists(election.id) {
+        election.phase = "Candidate";
+        elections[election.id] = election;
+    }
+
+    function readElection(string memory election_id) public view electionExists(election_id) returns(Election memory){
+        return elections[election_id];
+    }
+
+    /*
+    We do not do election updates
+    function updateElection(Election memory election) public electionExists(election.id) {
+        
+        
+    }
+    */
+
+    function deleteElection(string memory election_id) public electionExists(election_id) {
+        delete elections[election_id];
+    }
+
+    function endCandidate(string memory election_id) public electionExists(election_id) {
+        Election storage election = elections[election_id];
+        require(keccak256(bytes(election.phase)) == keccak256("Candidate"), "Not in phase 'Candidate'");
+        election.phase = "Registration";
+    }
+
+    function endRegistration(string memory election_id) public electionExists(election_id){
+        Election storage election = elections[election_id];
+        require(keccak256(bytes(election.phase)) == keccak256("Registration"), "Not in phase 'Registration'");
+        election.phase = "Voting";
+
+
+    }
+
+    function endVoting(string memory election_id) public electionExists(election_id){
+        Election storage election = elections[election_id];
+        require(keccak256(bytes(election.phase)) == keccak256("Voting"), "Not in phase 'Voting'");
+        election.phase = "Tally";
+
+
+    }
+
+    function endTally(string memory election_id) public electionExists(election_id){
+        Election storage election = elections[election_id];
+        require(keccak256(bytes(election.phase)) == keccak256("Tally"), "Not in phase 'Tally'");
+        election.phase = "Ended";
+    }
+
+}
