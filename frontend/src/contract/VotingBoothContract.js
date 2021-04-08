6a6650dd4861204df7c9b521a0d65a82f9714f11
Tjad Clark <tjataz@gmail.com>
2021-04-08 20:22:00 +0200

    frontend: Correct initialization of contract adaptor

--- frontend/src/contract/VotingBoothContract.js
+++ frontend/src/contract/VotingBoothContract.js
@@ -1,17 +1,16 @@
 import BaseContract from './BaseContract';
 
 export class VotingBoothContract extends BaseContract{
-    contract = null
 
     constructor(){
         super()
-        let Contract = require('web3-eth-contract');
-        this.contract = new Contract(this.config.voting_booth.abi_interface, this.config.voting_booth.address);
+        this.contract = new window.web3.eth.Contract(this.config.voting_booth.abi_interface, this.config.voting_booth.address);
     }
 
 
     submitEncryptedMessages(election_id, messages){
-        return this.contract.methods.submitEncryptedMessages(election_id, messages ).send({from: window.ethereum.selectedAddress, gas: 1000000});
+        console.log(messages)
+        return this.contract.methods.submitEncryptedMessages(election_id, messages ).send({from: window.ethereum.selectedAddress, gas: 5000000});
     }
 
     getEncryptedMessages(election_id){
@@ -26,11 +25,8 @@ export class VotingBoothContract extends BaseContract{
 }
 
 export class EncryptedMessage{
-    public_key = null
-    message = null
-
-    constructor( message, public_key){
-        this.public_key = public_key
+    constructor( message, blockchain_address){
+        this.blockchain_address = blockchain_address
         this.message = message
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,17 +9,47 @@ import {
   // EncryptedMessage
 } from './contract/VotingBoothContract'
 import WalletUtil from './util/wallet-util';
-
+import { ElectionCandidate, ElectionCandidateContract } from './contract/ElectionCandidateContract';
+import ElectionBallot from './ElectionBallot';
+import DecryptVote from './DecryptVote';
 let config = require('./config.json')
 
 let Contract = require('web3-eth-contract');
 
-Contract.setProvider(config.provider);
 
-
-window.contract = {
-  voting_booth: new VotingBoothContract(),
+async function loadWeb3() {
+  if (window.ethereum) {
+      await window.ethereum.enable();
+      window.web3 = new Web3(window.ethereum);
+    
+  }
 }
+
+loadWeb3().then(()=>{
+  
+
+
+  window.contract = {
+    voting_booth: new VotingBoothContract(),
+  }
+  //let y = new ElectionCandidate("Tjad","id1","tjkey","MyElection","imagehash","imageurl")
+
+  //window.contract.election_candidate.createCandidate("MyElection", y);
+
+  
+  // WalletUtil.getPublicKey().then((key)=>{
+  //   alert(key)
+  //   let encrypted_msg = WalletUtil.encrypt("Hello world!", key)
+  //   alert(encrypted_msg);
+
+  //   WalletUtil.decrypt(encrypted_msg).then((x)=>{
+  //     alert(x)
+  //   })
+
+  // })
+
+  
+})
 
 
 
@@ -31,20 +61,6 @@ ReactDOM.render(
 );
 
 
-async function loadWeb3() {
-  if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      window.ethereum.enable();
-  }
-}
-
-loadWeb3().then(()=>{
-
-  let x = new WalletUtil()
-  // log x to prevent warnings
-  console.log(x)
-  
-})
 
 
 // If you want to start measuring performance in your app, pass a function
