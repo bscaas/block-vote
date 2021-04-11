75eb8d12b5140de7d2a2d78dfffc904eb52b3b45
Jaiyeola Oluwademilade Mayowa <jaiyeolademilade@gmail.com>
2021-04-12 00:36:07 +0100

    require statements included

--- backend/ElectionCandidateContract.sol
+++ backend/ElectionCandidateContract.sol
@@ -14,23 +14,38 @@ pragma solidity >= 0.8.0;
         string url;
     }
 
-    mapping (string => mapping (string => CandidateInfo)) public candidates;
-       
-    function createCandidate(string memory _election_id, CandidateInfo memory _candidateinfo) public {
-        candidates[_election_id][ _candidateinfo.Candidate_ID] = _candidateinfo;
+     mapping (string => mapping (string => CandidateInfo)) public candidates;
+
+    modifier candidateNotExists(string memory _election_id, string memory _candidate_id ){
+         bytes memory _candidateIDbytes = bytes(_candidate_id);
+         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length == 0, "Candidate ID Already Exists");
+         require(_candidateIDbytes.length > 0, "Candidate ID was not passed");
+         _;
+     }
+      modifier candidateExists(string memory _election_id, string memory _candidate_id ){
+          bytes memory _candidateIDbytes = bytes(_candidate_id);
+         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length > 0, "Candidate ID Doesn't Exists");
+         require(_candidateIDbytes.length > 0, "Candidate ID was not passed");
+         _;
+      }
+
+
+   
+    function createCandidate(string memory _election_id, CandidateInfo memory _candidateinfo) public candidateNotExists(_election_id, _candidateinfo.Candidate_ID ) {
+     candidates[_election_id][ _candidateinfo.Candidate_ID] = _candidateinfo;
         
     }
 
-    function readCandidate(string memory _election_id, string memory _candidate_id)public view returns (CandidateInfo memory){
+    function readCandidate(string memory _election_id, string memory _candidate_id)public view candidateExists(_election_id, _candidate_id) returns (CandidateInfo memory){
        return candidates[_election_id][_candidate_id];
     }
     
-    function updateCandidate(string memory _election_id, CandidateInfo memory _candidateinfo) public{
+    function updateCandidate(string memory _election_id, CandidateInfo memory _candidateinfo) public candidateExists(_election_id, _candidateinfo.Candidate_ID){
            candidates[_election_id][_candidateinfo.Candidate_ID]= _candidateinfo;
          }
 
 
-    function deleteCandidate(string memory _election_id, string memory _candidate_id)public{
+    function deleteCandidate(string memory _election_id, string memory _candidate_id)public candidateExists(_election_id, _candidate_id){
            delete candidates[_election_id][_candidate_id];
     }
 
