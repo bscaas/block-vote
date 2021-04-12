3c66dec734687bc1075d9f4f08699a42a3860153
Jaiyeola Oluwademilade Mayowa <jaiyeolademilade@gmail.com>
2021-04-12 14:36:24 +0100

    Update ElectionCandidateContract.sol

--- backend/ElectionCandidateContract.sol
+++ backend/ElectionCandidateContract.sol
@@ -18,18 +18,17 @@ pragma solidity >= 0.8.0;
 
     modifier candidateNotExists(string memory _election_id, string memory _candidate_id ){
          bytes memory _candidateIDbytes = bytes(_candidate_id);
-         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length == 0, "Candidate ID Already Exists");
          require(_candidateIDbytes.length > 0, "Candidate ID was not passed");
+         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length == 0, "Candidate ID Already Exists");
          _;
      }
       modifier candidateExists(string memory _election_id, string memory _candidate_id ){
           bytes memory _candidateIDbytes = bytes(_candidate_id);
-         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length > 0, "Candidate ID Doesn't Exists");
          require(_candidateIDbytes.length > 0, "Candidate ID was not passed");
+         require(bytes(candidates[_election_id][_candidate_id].Candidate_ID).length > 0, "Candidate ID Doesn't Exists");
+        
          _;
       }
-
-
    
     function createCandidate(string memory _election_id, CandidateInfo memory _candidateinfo) public candidateNotExists(_election_id, _candidateinfo.Candidate_ID ) {
      candidates[_election_id][ _candidateinfo.Candidate_ID] = _candidateinfo;
