efc1fb4ad575a4bb868ed6f42ef2d08aad169ca0
Tjad Clark <tjataz@gmail.com>
2021-04-12 14:32:16 +0200

    #13 frontend: Add initial about page component

---
+++ frontend/src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class About extends React.Component{
+
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>This is all about Block Vote</div>
+        )
+    }
+} 
