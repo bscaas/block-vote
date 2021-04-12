43146d94cd48098cb1819dfc95f6f4d67b5a75f7
Tjad Clark <tjataz@gmail.com>
2021-04-12 14:31:58 +0200

    #14 frontend: Add initial home/landing page component

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Home extends React.Component{
+
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>Welcome to Block Vote</div>
+        )
+    }
+} 
