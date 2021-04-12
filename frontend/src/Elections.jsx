dc73cf65d81c0627902297ab441da3d5d2811b39
Tjad Clark <tjataz@gmail.com>
2021-04-12 19:43:04 +0200

    #11 frontend: Add phase as badge to election list

--- frontend/src/Elections.jsx
+++ frontend/src/Elections.jsx
@@ -7,8 +7,8 @@ export  class Elections extends React.Component{
     constructor(){
         super()
         this.elections = [
-            {name: 'Nigerian Election', id: GeneralUtil.uuidv4()},
-            {name: 'South African Election', id: GeneralUtil.uuidv4()}, 
+            {name: 'Nigerian Election', id: GeneralUtil.uuidv4(), phase: 'Vote'},
+            {name: 'South African Election', id: GeneralUtil.uuidv4(), phase: 'Candidate'}, 
         ]
     }
 
@@ -20,6 +20,7 @@ export  class Elections extends React.Component{
                     return(
                         <div className="election" onClick={()=>{this.editElection(election)}}>
                             {election.name}
+                            <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full float-right">{election.phase}</span>
                         </div>
                     )
                 })}
