3b648bca422f12069f1d71c34b57658f41ccd786
Donkross360 <mart2great@yahoo.com>
2021-04-12 21:34:03 +0100

    fixed the P tag for about page

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -9,9 +9,9 @@ export default class About extends React.Component{
         return(
             <div class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
                 <h2>BlockVote | Everything You Need To Know!</h2>
-                <P class="mt-2 text-gray-600">
+                <p class="mt-2 text-gray-600">
                     This Application is for election & polling at any scale.
-                </P>
+                </p>
             </div>
         )
     }
