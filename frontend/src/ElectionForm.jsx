9ddc32f8496eff9c50ab4959208755669e0801d3
Tjad Clark <tjataz@gmail.com>
2021-04-12 19:09:24 +0200

    #11 frontend: Distinguish between create and update on the election form and navigate from election list appropriately

--- frontend/src/ElectionForm.jsx
+++ frontend/src/ElectionForm.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import { Election } from './contract/ElectionContract'
 import GeneralUtil from './util/general-util'
-export default class ElectionForm extends React.Component{
+import { withRouter } from 'react-router-dom'
+export class ElectionForm extends React.Component{
 
     constructor(props){
         super()
-        this.election = new Election(GeneralUtil.uuidv4(),props.name);
+        this.election = props.location.state.election;
     }
 
     render(){
+
+        let button;
+        
+        if(this.election.id ){
+            button = <button onClick={this.updateElection}>Update</button>
+        }
+        else{
+            button = <button onClick={this.createElection}>Create</button>
+        }
+
         return(
             <div className="election">
                 <label>Name: </label>
-                <input type="text"/>
+                <input type="text" value={this.election.name} onChange={this.handleChangeName}/>
+                {button}                
             </div>
             
         )
     }
+
+    createElection = ()=>{
+        this.election.id = GeneralUtil.uuidv4()
+        alert(this.election.name + ' has been created with id '+ this.election.id)
+    }
+    updateElection = ()=>{
+        alert(this.election.name + ' has been updated')
+    }
+
+    handleChangeName = (event)=>{
+        this.election.name = event.target.value
+        this.setState({}) //Call setstate to re-render UI
+    }
 } 
+export default withRouter(ElectionForm);
--- frontend/src/Elections.jsx
+++ frontend/src/Elections.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import { useHistory } from "react-router-dom";
 import { withRouter } from 'react-router-dom';
+import GeneralUtil from './util/general-util';
 
 export  class Elections extends React.Component{
     
     constructor(){
         super()
         this.elections = [
-            {name: 'Nigerian Election'},
-            {name: 'South African Election'}, 
+            {name: 'Nigerian Election', id: GeneralUtil.uuidv4()},
+            {name: 'South African Election', id: GeneralUtil.uuidv4()}, 
         ]
     }
 
     render(){
         return(
             <div className="elections">
-                <i className="fas fa-plus-circle" onClick={this.createElection}></i>
+                <i className="fas fa-plus-circle" onClick={()=>{this.editElection({name: ''})}}></i>
                 {this.elections.map((election)=>{
                     return(
-                        <div className="election">
+                        <div className="election" onClick={()=>{this.editElection(election)}}>
                             {election.name}
                         </div>
                     )
@@ -28,8 +28,8 @@ export  class Elections extends React.Component{
         )
     }
 
-    createElection = ()=>{
-        this.props.history.push('/election-form')
+    editElection = (election)=>{
+        this.props.history.push('/election-form', {election: election })
     }
 } 
 export default withRouter(Elections);
