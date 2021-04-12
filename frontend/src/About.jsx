21722536516118fbc58948e9ae3e577742fc79d9
Donkross360 <mart2great@yahoo.com>
2021-04-12 22:05:18 +0100

    About updated

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -10,8 +10,9 @@ export default class About extends React.Component{
             <div class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
                 <h2>BlockVote | Everything You Need To Know!</h2>
                 <p class="mt-2 text-gray-500">
-                    This Application is for election & polling at any scale, also fortified with encrytion obfusication in order to obscure the voters from thier vote making it nearly imposible to see which candidate a voter voted.
-                <p class="mt-2 text-gray-500">The app has an inbuilt reward system to incentivise the voters.</p>
+                    This Application is for election & polling at any scale.
+                <p class="mt-2 text-gray-500">Fortified with encrytion obfusication in order to obscure the voters from thier vote making it nearly imposible to see which candidate a voter voted.</p>
+                <p class="mt-2 text-gray-500">These application has an inbuilt reward system to incentivise the voters. </p>
 
                 </p>
             </div>
