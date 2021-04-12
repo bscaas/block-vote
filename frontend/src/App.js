a0c2a34871802b59f2b940a5cd84ba49ffb6dcb5
Tjad Clark <tjataz@gmail.com>
2021-04-12 14:03:49 +0200

    #16 frontend: Make dynamic menu/router for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,64 +5,45 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import ElectionBallot from "./ElectionBallot";
+import DecryptVote from "./DecryptVote";
+import './App.css'
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+/*
+ MENU / ROUTER MAPPING
+*/
+const router_config = [
+  {name: "Ballot", path: "/ballot", component: ElectionBallot},
+  {name: "Decrypt", path: "/decrypt", component: DecryptVote},
+  {name: "Item 3", path: "/3"}
+]
 
+
+/*
+  APPLICATION
+
+*/
 export default function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/any">Any</Link>
-          </li>
+        <ul className="menu">
+            {router_config.map((item)=>{
+                return(<li>
+                    <Link to={item.path}>{item.name}</Link>
+                </li>)
+            })}
         </ul>
-
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/any">
-            <Any />
-          </Route>
+            {router_config.map((item)=>{
+                return (<Route exact path={item.path}>
+                    <h1>{item.name}</h1>
+                    {React.createElement(item.component)}
+                </Route>)
+            })}
+
         </Switch>
-      </div>
     </Router>
   );
 }
 
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function Any() {
-  return (
-    <div>
-      <h2>Any</h2>
-    </div>
-  );
-}
