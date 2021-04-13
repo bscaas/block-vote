2df18ae0b0850f07ce2cfc30e67edb7e14157a49
Tjad Clark <tjataz@gmail.com>
2021-04-13 22:09:32 +0200

    #15 frontend: correct storage of encryptions on IPFS + Blockchain

--- frontend/src/ElectionBallot.jsx
+++ frontend/src/ElectionBallot.jsx
@@ -68,30 +68,40 @@ export class ElectionBallot extends React.Component {
         
         let promises = []
         
+        let encryptions_1 = []
+        
         //Explode voter's candidate key for obfuscation of full vote
         for(let i=0;i<candidate_key.length;i++){
 
-            promises.push(new Promise(async (resolv, reject)=>{
 
-                let decryptor = this.state.decryptors[i%n_decryptors];
-                let pubkey = decryptor.public_key
-                let fragment = {
-                    vote_id: vote.id,
-                    candidate_key_fragment: candidate_key[i],
-                    candidate_key_fragment_position: i,
-                    election_id: candidate.election_id
-    
-                }
+            let decryptor = this.state.decryptors[i%n_decryptors];
+            let pubkey = decryptor.public_key
+            let fragment = {
+                vote_id: vote.id,
+                candidate_key_fragment: candidate_key[i],
+                candidate_key_fragment_position: i,
+                election_id: candidate.election_id
+
+            }
+            
+            encryptions_1.push(WalletUtil.encrypt(JSON.stringify(fragment), Buffer.from(pubkey)))
+
+            
+        }
+
+        let store_encryptions = async ()=>{
+            let results = await window.ipfs.add(encryptions_1)
+            let encryptions_2 = []
+            let i = 0
+            for await (const r of results){
+
                 
-                let encrypted_1 = WalletUtil.encrypt(JSON.stringify(fragment), Buffer.from(pubkey))
-    
-                //store encryption on ipfs as currently too expensive to store encrypted messages directly on the blockchain
+                let decryptor = this.state.decryptors[i%n_decryptors];
+
                 console.log("shooting file")
 
-                console.log(encrypted_1)
-                let buffer1 = uint8ArrayFromString(encrypted_1)
-                let result = await window.ipfs.add(buffer1)
-                let encryption1_cid = result[0].hash
+                
+                let encryption1_cid = r.path
                 let msg = {
                     blockchain_address: decryptor.blockchain_address,
                     message: encryption1_cid
@@ -99,25 +109,47 @@ export class ElectionBallot extends React.Component {
 
                 let decryptor2 = this.state.decryptors2[i%n_decryptors];
                 let pubkey2 = decryptor2.public_key
-                let encrypted_2 = WalletUtil.encrypt(JSON.stringify(msg), pubkey2)
+                
+                encryptions_2.push(WalletUtil.encrypt(JSON.stringify(msg), pubkey2))
 
-
-                let result2 = await window.ipfs.add(encrypted_2)
+                
                     
-                let encrypted2_cid =  result2[0].hash
-                let msg2 = new EncryptedMessage(encrypted2_cid,decryptor2.blockchain_address)
+                
 
-                encrypted_msgs.push(msg2)
-                resolv()
-            }))
+    
+
+                i = i+1
+            }    
+
+            let results2 = await window.ipfs.add(encryptions_2)
+            i=0
+            for await (const r2 of results2){
+                let decryptor2 = this.state.decryptors2[i%n_decryptors];
+
+                promises.push(new Promise(async (resolv, reject)=>{             
+                    let encrypted2_cid =  r2.path
+                    let msg2 = new EncryptedMessage(encrypted2_cid,decryptor2.blockchain_address)
+        
+                    encrypted_msgs.push(msg2)
+                    resolv()    
+                }))
+                i = i+1
+            }
+
         }
 
-        console.log("Before promises")
-        Promise.all(promises).then(()=>{
-            window.contract.voting_booth.submitEncryptedMessages(this.state.election_id, encrypted_msgs.slice(50))
-            console.log("after promises")
+        store_encryptions().then(()=>{
+
+            console.log("Before promises")
+            Promise.all(promises).then(()=>{
+                window.contract.voting_booth.submitEncryptedMessages(this.state.election_id, encrypted_msgs)
+                console.log("after promises")
+            })
         })
+    
+        
         
     }
 }
+
 export default withRouter(ElectionBallot);
