5d630ea1e0f9f56560beea7f671efe3bf19d23f0
Donkross360 <mart2great@yahoo.com>
2021-04-13 20:06:32 +0100

    added Logo to Homepage

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -8,9 +8,14 @@ export default class Home extends React.Component{
     render(){
         return(
             <div>
-                <h2 class="uppercase tracking-wide text-lg text-blue-600 font-bold">Welcome to Block Vote</h2>
-                <p class="mt-1 text-gray-600">Where Your Votes actually Counts!</p>
-
+                <div>
+                <img class="rounded-lg md:w-56" 
+                src={`${process.env.PUBLIC_URL}/assests/images/logo.png`} 
+                alt="logo"/>
+                
+                <h2 class="uppercase tracking-wide text-lg text-green-600 font-bold">Welcome to Block Vote</h2>
+                <p class="mt-1 text-gray-500">Where Your Votes actually Counts!</p>
+                </div>
             </div>
         )
     }
