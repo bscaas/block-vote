3eada8c048f9259a6ed6ce8601e675947f60d03f
Donkross360 <mart2great@yahoo.com>
2021-04-14 10:01:28 +0100

    assetFolder rename

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -10,7 +10,7 @@ export default class Home extends React.Component{
             <div>
                 <div>
                 <img class="rounded-lg md:w-56" 
-                src={`${process.env.PUBLIC_URL}/assests/images/logo.png`} 
+                src={`${process.env.PUBLIC_URL}/assets/images/logo.png`} 
                 alt="logo"/>
                 
                 <h2 class="uppercase tracking-wide text-lg text-green-600 font-bold">Welcome to Block Vote</h2>
