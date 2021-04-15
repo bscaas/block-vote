45f50da139c17e7a271e6f281b2c9a127108a458
Jaiyeola Oluwademilade Mayowa <jaiyeolademilade@gmail.com>
2021-04-15 16:50:40 +0100

    fixed algorithmic bug in allowance function

--- backend/LibertyToken.sol
+++ backend/LibertyToken.sol
@@ -57,7 +57,7 @@ contract LibertyToken {
         return true;
     }
 
-    function allowance(address _owner, address _spender, uint256 _value) public view returns(uint256){
+    function allowance(address _owner, address _spender) public view returns(uint256){
         return allowed[_owner][_spender];
     }
 
