3dd3764e1c02c6d0ad1e0be1147864042e796c3a
Tjad Clark <tjataz@gmail.com>
2021-04-17 09:41:02 +0200

    #22 frontend: Election Candidate ABI integration

--- frontend/src/contract/ElectionCandidateContract.js
+++ frontend/src/contract/ElectionCandidateContract.js
@@ -5,12 +5,26 @@ export class ElectionCandidateContract extends BaseContract{
 
     constructor(){
         super()
-        let Contract = require('web3-eth-contract');
         this.contract = new window.web3.eth.Contract(this.config.election_candidate.abi_interface, this.config.election_candidate.address);
     }
 
     createCandidate(election_id, candidate){
         return this.contract.methods.createCandidate(election_id, candidate ).send({from: window.ethereum.selectedAddress, gas: 1000000});
+    }
+
+
+    updateCandidate(election_id, candidate){
+        return this.contract.methods.updateCandidate(election_id, candidate ).send({from: window.ethereum.selectedAddress, gas: 1000000});
+    }
+
+
+    readCandidate(election_id, candidate_id){
+        return this.contract.methods.readCandidate(election_id, candidate_id ).send({from: window.ethereum.selectedAddress, gas: 1000000});
+    }
+
+
+    deleteCandidate(election_id, candidate_id){
+        return this.contract.methods.deleteCandidate(election_id, candidate_id ).send({from: window.ethereum.selectedAddress, gas: 1000000});
     }
 
 }
