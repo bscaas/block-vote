3e8994d7f6ff9f6e097d44f076fd4dd564af10ad
Tjad Clark <tjataz@gmail.com>
2021-04-20 17:07:18 +0200

    #33 frontend: Add ipfs file upload react component

---
+++ frontend/src/ipfs-upload.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { AppUtil } from './App';
+
+export default class IPFSUpload extends React.Component{
+    constructor(){
+        super()
+    }
+
+    handleChange = (event)=>{
+        // Update the state
+        this.selected_file = event.target.files[0]
+        this.setState({});
+    }
+
+    upload = async ()=>{
+        AppUtil.startLoading()
+        if(this.selected_file){
+            let bytes = await this.selected_file.arrayBuffer()
+            let results = await window.ipfs.add(bytes)
+
+            for await (const result of results){
+                this.cid = result.path            
+            }
+        }
+        else{
+            
+        }
+
+        AppUtil.stopLoading()
+    }
+
+    render(){
+        return(
+            <div>
+                <input type="file" onChange={this.handleChange} />
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={this.upload}>Upload</button>
+            </div>
+        
+        )
+    }
+}
