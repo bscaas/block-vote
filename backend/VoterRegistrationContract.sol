d26addbf551deafe10eba2a294d13a547eb50ce7
Oluwademilade Jaiyeola <jaiyeolademilade@gmail.com>
2021-04-21 13:11:14 +0100

    #7 incorparate Luhn Check, NIN & Reg address duplicte checks

--- backend/VoterRegistrationContract.sol
+++ backend/VoterRegistrationContract.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.0;
 
+import "./RegistrationUtility.sol";
+
 struct Voter {
     uint id;
     address blockchain_address; 
@@ -9,14 +11,24 @@ struct Voter {
     
 contract VoterRegistrationContract {
     
+    using RegistrationUtility for string;
+
     mapping(string=>uint) voter_counts;
-    mapping(string => mapping(uint => Voter)) voters;
-    
+    mapping(string=>mapping(uint => Voter)) voters;
+    mapping(string=>mapping(string=>bool)) private nins_exists;
+    mapping(string => mapping(address=>bool)) private blockchain_address_exists;
     
     constructor() public {
     }
     
     function registerVoter (string memory election_id, Voter memory voter) public {
+        require(!blockchain_address_exists[election_id][msg.sender], "Blockchain address already exists");
+        blockchain_address_exists[election_id][msg.sender] = true;
+        voter.blockchain_address = msg.sender;
+        string memory voter_nin = voter.nin;
+        require (RegistrationUtility.luhnCheck(voter_nin), "Invalid Nation Identity Number");
+        bytes memory bytesVoter_nin = bytes(voter.nin);
+        require(nins_exists[election_id][voter_nin] = true, "Voter National Identity Number already exists ");
         uint next_id = voter_counts[election_id];
         voter.id = next_id;
         voters[election_id][next_id] = voter;
